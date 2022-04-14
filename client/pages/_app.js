73b2142e5add74ad337a92728f866962b0412c9b
Максим Сухарев <tris-z@mail.ru>
2022-04-14 11:57:39 +0700

    move styles, add fonts

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../assets/styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../assets/styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -14,6 +14,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to Junior Gate!
         </h1>
+        <h2 className="cursive">
+          This is Quentin font
+        </h2>
       </main>
     </div>
   )
