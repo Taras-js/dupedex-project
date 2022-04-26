c4d599d84966923215ea42e5999910f5a01c1481
Taras Shevchenko <shev-taras@ya.ru>
2022-04-27 02:11:22 +0400

    feat: endpoint search by brand_name api/search

--- models/Products.js
+++ models/Products.js
@@ -1,14 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-// const productSchema = new Schema({
-//     id: { type: Number},
-//     brand_name: {type: String},
-//     prod_name: {type: String},
-//     prod_link: {type: String},
-//     img_link: {type: String},
-//     reviews: {type: Array},
-// });
+
 const productSchema = new Schema({
     id: { type: Number},
     brand_name: {type: String},
--- routes/data.routes.js
+++ routes/data.routes.js
@@ -17,8 +17,8 @@ router.get("/data",  async (req, res) => {
 router.get("/data/reviews",  async (req, res) => {
 
     try {
-        const products = await Product.find({}, {id: 1, reviews: 1
-        }).limit(10).lean();
+        const products = await Product.find({}, {id: 1, brand_name: 1
+        }).lean();
         res.json(products);
     } catch (error) {
         res.status(500).json({ message: "Что-то пошло не так" });
@@ -31,6 +31,19 @@ router.get("/data/:id", async (req, res) => {
         res.json(product);
     } catch (error) {
         res.status(500).json({ message: "Что-то пошло не так" });
+    }
+});
+router.post("/search",  async (req, res) => {
+
+    try {
+        let payload = req.body.payload.trim()
+        let search = await Product.find({brand_name: {$regex: new RegExp('^'+payload+'.*','i')}}, {brand_name: 1}).limit(10).exec()
+
+        search = search.slice(0, 10)
+        res.json(search);
+    } catch (error) {
+        res.status(500).json({ message: "Что-то пошло не так" });
+        console.log('error')
     }
 });
 module.exports = router
