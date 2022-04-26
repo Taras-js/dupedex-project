716aa93115810358a61dbf7ea99d418c50e4edad
Taras Shevchenko <shev-taras@ya.ru>
2022-04-26 22:54:08 +0400

    feat: endpoint 'api/data' method 'GET' && 'api/data/reviews' method 'GET' 'api/data/:id'

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -5,12 +5,32 @@ const router = Router();
 router.get("/data",  async (req, res) => {
 
     try {
-        const products = await Product.find();
+        const products = await Product.find({}, {id: 1, brand_name: 1,
+    prod_name: 1, prod_link: 1, img_link: 1, reviews: 1
+    }).limit(5).lean();
         res.json(products);
     } catch (error) {
         res.status(500).json({ message: "Что-то пошло не так" });
         console.log('error')
     }
 });
+router.get("/data/reviews",  async (req, res) => {
 
+    try {
+        const products = await Product.find({}, {id: 1, reviews: 1
+        }).limit(10).lean();
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ message: "Что-то пошло не так" });
+        console.log('error')
+    }
+});
+router.get("/data/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.json(product);
+    } catch (error) {
+        res.status(500).json({ message: "Что-то пошло не так" });
+    }
+});
 module.exports = router
