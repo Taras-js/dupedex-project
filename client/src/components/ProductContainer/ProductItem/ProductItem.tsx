833295fab6e10262af99acd7d4aa1db2edb22d8c
Vlad7is7lav <vladsydneyl@gmail.com>
2022-04-29 21:59:50 +0300

    feat: add color generator

--- client/src/components/ProductContainer/ProductItem/ProductItem.tsx
+++ client/src/components/ProductContainer/ProductItem/ProductItem.tsx
@@ -16,6 +16,31 @@ function ProductItem(props: ProductProps) {
 
   const reviews = getReviews(item.reviews);
 
+  //function to apply color for tag
+  const fillTagBackground = (review:string, score:number):string => {
+    let alpha;
+    let re = /\((.*?)[\)|\/]/g;
+    let checkMark = re.exec(review);
+    let mark = !checkMark ? "other" : checkMark[1] ;
+
+    enum Marks {
+      positive = "28, 191, 96",
+      negative = "251, 120, 142",
+      neutral = "254, 226, 148",
+      other = "240, 240, 240"
+    }
+
+    switch (true) {
+      case(mark === "other" || !Marks[mark]): alpha = 1; break;
+      case(score > 13): alpha = 1; break;
+      case(score > 7): alpha = 0.75; break;
+      case(score > 5): alpha = 0.55; break;
+      case(score > 3): alpha = 0.35; break;
+      case(score > 0): alpha = 0.2; break;
+    }
+    return `rgba(${ !Marks[mark] ? Marks["other"] : Marks[mark] }, ${alpha})`
+  }
+
   return (
     <div key={id} className={styles.products__item}>
       <a href={item.prod_link}><img className={styles.img__link} src={item.img_link} alt={item.prod_link} /></a>
@@ -33,7 +58,7 @@ function ProductItem(props: ProductProps) {
       {reviews
         .filter((review) => (review[1] > 1))
         .map((review) => (
-          <div>
+          <div style={{backgroundColor: fillTagBackground(review[0], Number(review[1])), padding: "8px 12px", borderRadius: "9px", margin: "5px", minWidth: "160px", textAlign: "center"}}>
             <span>{review[0]}</span>
             {': '}
             <span>{Number(review[1]).toFixed(2)}</span>
