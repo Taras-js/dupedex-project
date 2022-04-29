1aba8a308d0377dd4b681fd3ba6627bef8ece39e
Taras Shevchenko <shev-taras@ya.ru>
2022-04-29 16:47:52 +0400

    feat: endpoint search by brand_name api/search

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -37,7 +37,9 @@ router.post("/search",  async (req, res) => {
 
     try {
         let payload = req.body.payload.trim()
-        let search = await Product.find({brand_name: {$regex: new RegExp('^'+payload+'.*','i')}}, {brand_name: 1}).limit(10).exec()
+        let search = await Product.find({brand_name: {$regex: new RegExp('^'+payload+'.*','i')}},
+            {brand_name: 1, prod_name: 1, prod_link: 1, price: 1, category: 1, img_link: 1, Benefits: 1,
+                Details: 1, Usage: 1, Ingredients: 1, reviews: 1, key_ingredients: 1,}).limit(10).exec()
 
         search = search.slice(0, 10)
         res.json(search);
