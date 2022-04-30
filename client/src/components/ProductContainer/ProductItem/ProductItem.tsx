ce5b495613e226a18b2b8e52161b892b5e3252bf
Vlad7is7lav <vladsydneyl@gmail.com>
2022-04-30 21:02:30 +0300

    fix: change color function and add color for text

--- client/src/components/ProductContainer/ProductItem/ProductItem.tsx
+++ client/src/components/ProductContainer/ProductItem/ProductItem.tsx
@@ -1,6 +1,6 @@
 import { productsMock } from '../../../shared/mocks/productmock';
 import { Filter } from '../../../shared/types';
-import { getReviews } from '../../../utils/helper';
+import { getReviews, getTagStyle } from '../../../utils/helper';
 
 import styles from './productItem.module.css';
 
@@ -9,38 +9,26 @@ interface ProductProps {
   filter?: Filter;
 }
 
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+interface getTagStyle {
+  backgroundColor: RGBA,
+  color: string
+}
+
+enum Marks {
+  positive = "28, 191, 96",
+  negative = "251, 120, 142",
+  neutral = "254, 226, 148",
+  other = "140, 140, 140"
+}
+
 function ProductItem(props: ProductProps) {
   const { id, filter } = props;
-
+  
   const item = productsMock.find((itemToFind) => itemToFind.id === id);
 
   const reviews = getReviews(item.reviews);
-
-  //function to apply color for tag
-  const fillTagBackground = (review:string, score:number):string => {
-    let alpha;
-    let re = /\((.*?)[\)|\/]/g;
-    let checkMark = re.exec(review);
-    let mark = !checkMark ? "other" : checkMark[1] ;
-
-    enum Marks {
-      positive = "28, 191, 96",
-      negative = "251, 120, 142",
-      neutral = "254, 226, 148",
-      other = "240, 240, 240"
-    }
-
-    switch (true) {
-      case(mark === "other" || !Marks[mark]): alpha = 1; break;
-      case(score > 13): alpha = 1; break;
-      case(score > 7): alpha = 0.75; break;
-      case(score > 5): alpha = 0.55; break;
-      case(score > 3): alpha = 0.35; break;
-      case(score > 0): alpha = 0.2; break;
-    }
-    return `rgba(${ !Marks[mark] ? Marks["other"] : Marks[mark] }, ${alpha})`
-  }
-
+  
   return (
     <div key={id} className={styles.products__item}>
       <a href={item.prod_link}><img className={styles.img__link} src={item.img_link} alt={item.prod_link} /></a>
@@ -58,7 +46,7 @@ function ProductItem(props: ProductProps) {
       {reviews
         .filter((review) => (review[1] > 1))
         .map((review) => (
-          <div style={{backgroundColor: fillTagBackground(review[0], Number(review[1])), padding: "8px 12px", borderRadius: "9px", margin: "5px", minWidth: "160px", textAlign: "center"}}>
+          <div style={getTagStyle(review[0], Number(review[1]))} className={styles.tag__item}>
             <span>{review[0]}</span>
             {': '}
             <span>{Number(review[1]).toFixed(2)}</span>
--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -26,3 +26,41 @@ export function getReviews(reviews: Array<any>): Array<any> {
 
   return sort(Object.entries(objReviews), 1);
 }
+
+//function to apply color for tag background and text
+interface getTagStyle {
+  backgroundColor: RGBA,
+  color: string
+}
+
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+
+enum Marks {
+  positive = "28, 191, 96",
+  negative = "251, 120, 142",
+  neutral = "254, 226, 148",
+  other = "170, 170, 170"
+}
+
+export const getTagStyle = (review:string, score:number):getTagStyle => {
+  let tagColor:string,
+      alpha:number;
+  let re:RegExp = /\((.*?)[\)|\/]/g;
+  let findMood:RegExpExecArray = re.exec(review.split(" ").pop());
+  let color:Marks = !findMood ? Marks["other"] : Marks[findMood[1]];
+
+  alpha = getAplhaFromScore(score, color)
+  tagColor = (color === Marks["other"]) ? "#FFFFFF" : alpha > .6 ? "#FFF" : "111";
+  return {backgroundColor: `rgba(${color}, ${alpha})`, color: tagColor}
+}
+
+const getAplhaFromScore = (score:number, color:string):number => {
+  switch (true) {
+    case(color === Marks["other"]): return 1; break;
+    case (score > 13): return 1; break;
+    case (score > 7): return 0.75; break;
+    case (score > 5): return 0.55; break;
+    case (score > 3): return 0.35; break;
+    case (score > 0): return 0.2; break;
+  }
+}
