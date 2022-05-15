ceacd97437ba0890a15aa01a091d6d136778689b
Taras Shevchenko <shev-taras@ya.ru>
2022-05-15 22:01:29 +0400

    feat: add input from click and style

--- client/src/features/modals/ModalLogin/ModalLogin.tsx
+++ client/src/features/modals/ModalLogin/ModalLogin.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./modalLogin.module.css";
 import { Button } from "../../../components/UIKit";
 
 const ModalLogin: React.FC = () => {
+  const [input, setInput] = useState(true)
   const onMobileLoginClick = () => {
-    console.log("onShareClick click");
+    setInput(false);
   };
-
+  const container = {
+    padding: 12,
+    marginBottom: 10,
+    borderRadius: 7,
+    background: "rgba(255, 195, 200, 1)",
+    textAlign: "center",
+    width: 324,
+    height: 59
+  }
   const onEmailLoginClick = () => {
     console.log("onMessageClick click");
   };
+  const sendSms = () => {
+    console.log("send Sms");
+  };
+
   return (
     <>
       <div className={styles.modal__login}>
@@ -19,28 +32,47 @@ const ModalLogin: React.FC = () => {
         </p>
 
         <div className={styles.modal__content}>
-          <Button
-            icon
-            isDisabled={false}
-            className={"btn"}
-            onClick={onEmailLoginClick}
-          >
-            <div className={styles.modal__content_email}>
-              Continue with email (coming soon)
-            </div>
-          </Button>
-          <Button
-            icon
-            isDisabled={false}
-            className={"btn"}
-            onClick={onMobileLoginClick}
-          >
-            <div className={styles.modal__content_mobile}>
-              Continue with mobile
-            </div>
-          </Button>
+          {input
+          ?
+              <>
+                <Button
+                    icon
+                    isDisabled={true}
+                    className={"btn"}
+                    onClick={onEmailLoginClick}
+                >
+                  <div className={styles.modal__content_email}>
+                    Continue with email (coming soon)
+                  </div>
+                </Button>
+                <Button
+                    icon
+                    isDisabled={false}
+                    className={"btn"}
+                    onClick={onMobileLoginClick}
+                ><div className={styles.modal__content_mobile}>
+                  Continue with mobile
+                </div>
+                </Button>
+              </>
+                :
+              <>
+                <input
+                    style={container}
+                    maxLength={14}
+                />
+                <Button
+                    icon
+                    isDisabled={false}
+                    className={"btn"}
+                    onClick={sendSms}
+                ><div className={styles.modal__content_mobile}>
+                  Send message
+                </div>
+                </Button>
+              </>
+          }
         </div>
-
         <p className={styles.modal__text}>
           By continuing, you agree with our{" "}
           <a href={"#"}>
